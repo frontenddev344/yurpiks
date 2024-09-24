30eb42e303441a9696a939c6cafd26608d82cece
Dev avology <devavology12@gmail.com>
2024-09-24 16:46:29 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,26 @@ AOS.init();
 // AOS JS Start
 
 
+// Singup/SignIn JS Start
+function toggleSignup(){
+  document.getElementById("login-toggle").style.backgroundColor="#fff";
+   document.getElementById("login-toggle").style.color="#222";
+   document.getElementById("signup-toggle").style.backgroundColor="#57b846";
+   document.getElementById("signup-toggle").style.color="#fff";
+   document.getElementById("login-form").style.display="none";
+   document.getElementById("signup-form").style.display="block";
+}
+
+function toggleLogin(){
+   document.getElementById("login-toggle").style.backgroundColor="#57B846";
+   document.getElementById("login-toggle").style.color="#fff";
+   document.getElementById("signup-toggle").style.backgroundColor="#fff";
+   document.getElementById("signup-toggle").style.color="#222";
+   document.getElementById("signup-form").style.display="none";
+   document.getElementById("login-form").style.display="block";
+}
+// Singup/SignIn JS End
+
 
 //Cookies JS Start
 const cookiesBox = document.querySelector('.wrapper'),
